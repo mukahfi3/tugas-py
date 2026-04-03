648d0c76195147716833f96255efee32a19eb5f3
MartabakTelor <mukahfi3@gmail.com>
2026-04-03 10:01:32 +0700

    Update latihan2.py

--- latihan2.py
+++ latihan2.py
@@ -1,1 +1,8 @@
+x = int(input("Masukkan jumlah hari: "))
 
+tahun = x // 365
+sisa  = x % 365
+bulan = sisa // 30
+hari  = sisa % 30
+
+print(f"{x} hari = {tahun} tahun, {bulan} bulan, {hari} hari")
